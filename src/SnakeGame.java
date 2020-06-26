f7132064207d3e34ab971f0d113d9d48301b6a0d
armintalaie <talaiearmin78@gmail.com>
2020-06-25 21:46:38 -0700

    set up GUI window for game

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -3,8 +3,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class SnakeGame extends Application {
 
