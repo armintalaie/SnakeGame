1222a9b5451bbfe386b8c42f64d978d2bf592471
armintalaie <armin.talaie@alumni.ubc.ca>
2020-06-25 21:33:55 -0700

    check code

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,6 +1,9 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SnakeGame extends Application {
 
     public static void main(String[] args) {
@@ -10,5 +13,29 @@ public class SnakeGame extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        int min = -1;
+        int length = 200;
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> edges = new ArrayList<>();
+
+        for (int i = 0; i < length; i++) {
+            String[] line = scanner.nextLine().split("\\s+");
+
+
+            // System.out.println(Arrays.toString(line));
+            int first = Integer.parseInt(line[0]);
+
+            for (int j = 1; j < line.length; j++) {
+                Integer temp = 0;
+
+                if (!edges.contains(temp)) {
+                    edges.add(temp);
+                }
+
+            }
+
+
+        }
     }
 }
