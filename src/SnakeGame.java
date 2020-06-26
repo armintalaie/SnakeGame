f3b5fb64b41cc0547b4d67ebbd28934ae2e9933e
armintalaie <armin.talaie@alumni.ubc.ca>
2020-06-25 21:24:34 -0700

    snake game in java because I wanted to added faster and a better GUI

---
+++ src/SnakeGame.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SnakeGame extends Application {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
