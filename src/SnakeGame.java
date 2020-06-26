88ca98a500540f9fb3d36ef5a805d2139722ac1c
armintalaie <armin.talaie@alumni.ubc.ca>
2020-06-25 21:40:52 -0700

    set up GUI window for game

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -6,36 +8,26 @@ import java.util.Scanner;
 
 public class SnakeGame extends Application {
 
-    public static void main(String[] args) {
 
-    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        int min = -1;
-        int length = 200;
-        Scanner scanner = new Scanner(System.in);
+        GridPane pane = new GridPane();
 
-        ArrayList<Integer> edges = new ArrayList<>();
+        Scene scene = new Scene(pane, 1200, 800);
 
-        for (int i = 0; i < length; i++) {
-            String[] line = scanner.nextLine().split("\\s+");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
 
-            // System.out.println(Arrays.toString(line));
-            int first = Integer.parseInt(line[0]);
-
-            for (int j = 1; j < line.length; j++) {
-                Integer temp = 0;
-
-                if (!edges.contains(temp)) {
-                    edges.add(temp);
-                }
-
-            }
 
 
-        }
+
+
+
+
+
+
     }
 }
